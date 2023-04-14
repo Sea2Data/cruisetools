b399419f6bbe8ffd01aa24b933970509fd9e10b8
Handegard <nilsolav@hi.no>
2023-04-14 12:48:52 +0200

    Rewritten to new API version and RstoxData

--- NMDbioticprogress/trawlstationspercruiseseries.R
+++ NMDbioticprogress/trawlstationspercruiseseries.R
@@ -1,81 +1,60 @@
 # This script generates an overview of stations in NMDbiotic with snapshots
 
 # set up environment
-library(Rstox)
+library(RstoxData)
 library(XML)
 library(httr)
 library(jsonlite)
 
 # Set parameters
-setwd('D:/repos/Github/cruisetools/NMDbioticprogress')
-base <- "http://tomcat7.imr.no:8080/apis/nmdapi/biotic/v3"
+setwd('C:/repos/cruisetools/NMDbioticprogress')
+base <- "http://tomcat7.imr.no:8080/apis/nmdapi/biotic/v3/"
+
+#http://tomcat7.imr.no:8080/apis/nmdapi/biotic/v3/Forskningsfart??y/2017/G.O.Sars_LMEL/2017150/snapshot?version=3.0
 
 year0 = 2007
+data = NULL
+# Loop over mission types
+missions = GET(URLencode("http://tomcat7.imr.no:8080/apis/nmdapi/biotic/v3?type=ListAll"))
+datasets <- xmlToList(xmlParse(rawToChar(missions$content)))
 
-# List *all* missions @ NMD
-CS <- getNMDinfo("c", recursive=FALSE)
-# Remove any data newer than 2015 (since they are using NMDbiotic as the primary system
-C <- CS[CS$startyear<2015,]
-# Reduse data for testing...
-#C <- C[C$startyear==year0,]
-C <- as.data.frame(C)
-
-#C[i,4]$missiontype,C[i]$startyear,C[i]$platform)
-I <- dim(C)
-data <- NULL
-
-for (i in seq(1,I[1])){
-#for (i in seq(1,2)){
-  print(paste("Mission ",i,' of ',I[1]))
-  # Get "platformpath" (add call signal if it exist)
-  if(is.na(C[i,17])){platformpath <- paste0(C[i,7])}
-  else {platformpath <- paste0(C[i,7],"_",C[i,17])}
+# Loop over datasets
+for (dataset in datasets){
+  # Get path to biotic data
+  for (dataset_ in dataset){if (dataset_$.attrs=='path'){platformpath<-dataset_$text}}
   
-  # Get "delivery" (check if the mission has cruise number or not)
-  if(is.na(C[i,18])){delivery <- C[i,3]}
-  else {delivery <- C[i,18]}
-  
-  # Get "missiontype"
-  missiontype <- C[i,5]
-  
-  # Get "year"
-  year <- C[i,11]
-  
-  # Get the list of snapshots from the API
-  #/{missiontype}/{year}/{platform}/{delivery}/snapshot
-  snsht <- paste0(base,"/",missiontype,"/",year,"/",platformpath,"/",delivery,"/snapshot?version=3.0")
-  snapshots_raw<- xmlParse(URLencode(snsht))
-  snapshots <- xmlToList(snapshots_raw)
-  
+  # List snapshots for this delivery
+  snapshotURL <- paste0(base,platformpath,'/snapshot/')
+  ss = GET(URLencode(snapshotURL))
+  snapshots <- xmlToList(xmlParse(rawToChar(ss$content)))
   # Loop over snapshots
-  for (j in seq(1,length(snapshots))){ 
+  for (snapshot in snapshots){
     # Get NMDbiotic snapshot URL and download data
-    #/{missiontype}/{year}/{platform}/{delivery}/snapshot/{time}    
-    snapshottime <- snapshots[j]$row$element$text
-    biotic_url <- URLencode(paste0(base,"/",missiontype,"/",year,"/",platformpath,"/",delivery,"/snapshot/",snapshottime,"?version=3.0"))
+    #/{missiontype}/{year}/{platform}/{delivery}/snapshot/{time}
+    snapshottime <- snapshot$element$text
+    URL_sn <- paste0(snapshotURL,snapshottime)
+    print(URL_sn)
     # Download biotic file
-    download.file(biotic_url,destfile='biotic_temp.xml',quiet = T)
-    # Read the biotic file
-    biotic <- readXMLfiles('biotic_temp.xml')
+    download.file(URLencode(URL_sn),destfile='biotic_temp.xml',quiet = T)
     
-    # check if the data is NULL
-    fs_dim <- dim(biotic$ReadBioticXML_BioticData_fishstation.txt)
-    if (!fs_dim[1]==0){
-      # Create data frame 
-      serialnumber <- (biotic$ReadBioticXML_BioticData_fishstation.txt$serialnumber)
-      datasub <- as.data.frame(serialnumber)
-      datasub$snapshotpath = snapshottime
-      #datasub$snapshottime = as.POSIXct(snapshots[j]$row$element$text,format = "%Y-%m-%dT%H.%M.%OSZ")
-      datasub$platformpath = platformpath
-      datasub$delivery = delivery
-      datasub$missiontype = missiontype
-      datasub$year = year
-      # Coerce to main data frame
-      data <- rbind.data.frame(data,datasub)
+    # Read the biotic file
+    biotic <- ReadBiotic('biotic_temp.xml')
+    serialnumber <- biotic$biotic_temp.xml$fishstation$serialnumber
+    if (length(serialnumber)>0){
+    # Create data frame 
+    datasub <- as.data.frame(serialnumber)
+    datasub$snapshotpath = snapshottime
+    #datasub$snapshottime = as.POSIXct(snapshots[j]$row$element$text,format = "%Y-%m-%dT%H.%M.%OSZ")
+    datasub$platformpath = URL_sn
+    #datasub$delivery = delivery
+    #datasub$missiontype = missiontype
+    #datasub$year = year
+    # Coerce to main data frame
+    data <- rbind.data.frame(data,datasub) 
     }
   }
 }
-# Write data frame to disk
-head(data)
+# # Write data frame to disk
+# head(data)
 write.table(data, file = 'NMDbiotic.csv', sep = ";", row.names = F, fileEncoding = "UTF-8")
-?write.table
+# ?write.table
